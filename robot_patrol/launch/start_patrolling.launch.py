e9cef5ab90b53ec8e527b820082f35f85302c597
Pan <pan.techprototype@gmail.com>
2025-09-29 15:00:41 +0000

    Add code to ;aunch rviz and add a rviz file name robot.rviz

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -1,12 +1,28 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    # Get path to the RViz config inside your package
+    rviz_config_path = os.path.join(
+        get_package_share_directory('robot_patrol'),
+        'rviz',
+        'robot.rviz'
+    )
+
     return LaunchDescription([
         Node(
             package='robot_patrol',
             executable='robot_patrol_node',
             output='screen',
-            emulate_tty=True),
+            emulate_tty=True
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_path],
+            output='screen'
+        )
     ])
